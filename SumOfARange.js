45bb881d0717759619caa1fe3158cdb302890af1
Zac Cogley <zaccogley@gmail.com>
2017-03-04 12:06:00 -0600

    bonus completed

--- SumOfARange.js
+++ SumOfARange.js
@@ -1,13 +1,13 @@
 /*Write a range function that takes two arguments, start and end, and
 returns an array containing all the numbers from start up to (and including) end.*/
 
-function range (start, end){
-  var numbers = []
-  for (var i = start; i <= end; i++){
-    numbers.push(i)
-  }
-  return numbers
-}
+// function range (start, end){
+//   var numbers = []
+//   for (var i = start; i <= end; i++){
+//     numbers.push(i)
+//   }
+//   return numbers
+// }
 
 /*Next, write a sum function that takes an array of numbers and returns the sum of
 these numbers. Run the previous program and see whether it does indeed return 55.*/
@@ -24,3 +24,22 @@ If no step is given, the array elements go up by increments of one, correspondin
 to the old behavior. The function call range(1, 10, 2) should return
 [1, 3, 5, 7, 9]. Make sure it also works with negative step values so that
 range(5, 2, -1) produces [5, 4, 3, 2].*/
+
+
+function range (start, end, step){
+  var numbers = []
+  if (typeof step != "number")
+  	for (var i = start; i <= end; i++){
+      numbers.push(i)
+    };
+
+  if (step > 0)
+    for (var i = start; i <= end; i = i + step){
+    numbers.push(i)
+      };
+  if (step < 0)
+    for (var i = start; end <= i; i = i + step){
+    numbers.push(i)
+      };
+  return numbers
+}
